461d598e9c27d92f534676fe11cdd8f5707c4146
Leslie Lopez <leslie2515lopez@gmail.com>
2021-10-13 19:35:54 -0500

    Cleaned up buttons CSS

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -1,23 +1,26 @@
-import styled from 'styled-components';
-import {Link} from 'react-scroll';
+import styled from "styled-components";
+import { Link } from "react-scroll";
 
 export const Button = styled(Link)`
-  border-radius: 50px;
-  background: ${({primary}) => (primary ? '#01bf71' : '#010606')};
-  white-space: nowrap;
-  padding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
-  color: ${({dark}) => (dark ? '#010606' : '#fff')};
-  font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
-  outline: none;
-  border: none;
-  cursor: pointer;
   display: flex;
   justify-content: center;
   align-items: center;
+  color: ${({ dark }) =>
+    dark ? "hsl(240,100%,98.6%)" : "hsl(240,100%,98.6%)"};
+  background: ${({ primary }) =>
+    primary ? "hsl(240,100%,40.2%)" : "hsl(276.5,100%,9%)"};
+  font-size: ${({ fontBig }) => (fontBig ? "1.25rem" : "1rem")};
+  padding: ${({ big }) => (big ? "0.87rem 3rem" : "0.75rem 1.87rem")};
+  border: none;
+  outline: none;
+  border-radius: 3.12rem;
+  white-space: nowrap;
   transition: all 0.2s ease-in-out;
+  cursor: pointer;
 
   &:hover {
-      transition: all 0.2s ease-in-out;
-      background: ${({primary}) => (primary ? '#fff' : '#01bf71')};
+    transition: all 0.2s ease-in-out;
+    background: ${({ primary }) =>
+      primary ? "hsl(270,50%,60%)" : "hsl(210.1,100%,50%)"};
   }
 `;
